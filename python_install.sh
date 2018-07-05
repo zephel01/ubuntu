475c5c3916c5c6056daaf44dca4263d59b8bd736
zephel01 <zephel01@gmail.com>
2018-07-05 19:14:44 +0900

    修正

--- python_install.sh
+++ python_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# delete us-east1.ec2.
+sudo sed -i 's/us-east-1\.ec2\.//g' /etc/apt/sources.list
+
 # 開発環境インストール
+sudo apt-get update
 sudo apt-get install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
 
 # pyenv install
@@ -12,9 +16,6 @@ sudo mkdir shims
 sudo mkdir versions
 
 sudo chown -R ubuntu:ubuntu /usr/bin/.pyenv
-
-cd ~
-sed -i -e 's/alias python/#alias python/g' ~/.bashrc
 
 cat ~/ubuntu/bashrc_add >> ~/.bashrc
 
