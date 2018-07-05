9983143038ba891d62d039f19c9c4e69c31bae32
zephel01 <zephel01@gmail.com>
2018-07-05 21:54:17 +0900

    add vnc service

--- vnc_install.sh
+++ vnc_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#ssh password auth on
-sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo apt-get install ubuntu-desktop
+sudo apt-get install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
 
-sudo /etc/init.d/ssh restart
-sudo passwd ubuntu
+sudo apt-get install vnc4server
 
-sudo apt install xrdp xfce4 xfce4-goodies tightvncserver
+vncserver
 
-echo xfce4-session> /home/ubuntu/.xsession
-sudo cp /home/ubuntu/.xsession /etc/skel
-sudo sed -i '0,/-1/s//ask-1/' /etc/xrdp/xrdp.ini
+cp ~/ubuntu/xstartup ~/.vnc/xstartup
 
-sudo service xrdp restart
+sudo cp ~/ubuntu/vncserver@.service /etc/systemd/system
+sudo systemctl daemon-reload
 
+sudo systemctl enable vncserver@1.service
+sudo systemctl list-unit-files | grep vnc
