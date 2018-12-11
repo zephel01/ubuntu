d1aacf2879568a2b796528ddbb7b4b62d5e6a1d8
zephel01 <zephel01@gmail.com>
2018-12-11 22:15:09 +0900

    update.sh

---
+++ python_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pyenv update
+
+cd ~/.pyenv
+git fetch --all
+git pull
+
+pyenv install 3.6.7
+pyenv global 3.6.7
+
+pip install --upgrade pip
+
+## pip install
+pip install -r ./requirements.txt
+
